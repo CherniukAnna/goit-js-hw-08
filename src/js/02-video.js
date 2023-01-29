3d04cbe452f71d26c4d100374ca33e25641d5b4f
CherniukAnna <chernukanna@gmail.com>
2023-01-29 17:59:53 +0200

    2 та 3 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+// player.on(
+//   'timeupdate',
+//   throttle(function (e) {
+//     localStorage.setItem('videoplayer-current-time', e.seconds);
+//   }, 1000)
+// );
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+//
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle';
 
+const emailInputEl = document.querySelector('input');
+const msgInputEl = document.querySelector('textarea');
+const formEl = document.querySelector('form');
+
+const formBtm = document.querySelector('button');
+const STORAGE_KEY = 'feedback-form-state';
+
+populateTextarea();
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log('send form');
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({
+      email: formEl.email.value,
+      message: formEl.message.value,
+    })
+  );
+}
+
+// function populateTextarea() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+//   if (savedMessage) {
+//     console.log(savedMessage);
+//     msgInputEl.value = savedMessage;
+//     emailInputEl.value = savedMessage;
+//   }
+// }
+
+// //
+
+function populateTextarea() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    emailInputEl.value = savedData.email.trim();
+    msgInputEl.value = savedData.message.trim();
+  }
+}
+
+//
